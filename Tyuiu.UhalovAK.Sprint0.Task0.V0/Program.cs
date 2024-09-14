b43bf2e6f09c9306a98dc4aa9a17e2c5d4155e3c
akukh <akukh@DESKTOP-6T6UKL1>
2024-09-14 22:16:15 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.UhalovAK.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.UhalovAK.Sprint0.Task0.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
