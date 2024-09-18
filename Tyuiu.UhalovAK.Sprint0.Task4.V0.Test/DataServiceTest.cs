d2071bec755a1783bdcf09f9b4332fad11b980ef
Uhalov Arseny <akukhalov@gmail.com>
2024-09-19 01:30:41 +0500

    Task 4

---
+++ Tyuiu.UhalovAK.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+using Tyuiu.UhalovAK.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.UhalovAK.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValied()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValied()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValied()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValied()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+
+
+    }
+}
