5b1c83cfaae9e58c8b21398ebf0933fe73a01fda
Uhalov Arseny <akukhalov@gmail.com>
2024-09-20 00:36:58 +0500

    Task6

---
+++ Tyuiu.UhalovAK.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.UhalovAK.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.UhalovAK.Sprint0.Task6.V0
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadLine();
+
+        }
+    }
+}
